3185b88f2bf8cdd9b3949cd106fa76df6eb180fa
danakendybayeva <danka.kent@gmail.com>
2020-02-13 12:12:55 +0600

    week4

---
+++ week4/my-app/src/app/products.ts
@@ -0,0 +1,109 @@
+export const products = [
+  {
+    image1: 'assets/1.jpg',
+    image2: 'assets/1_2.jpg',
+    image3: 'assets/1_3.jpg',
+    name: 'Flower Stickers Pack',
+    price: '$0.14',
+    description: '45pcs/box DIY Posted It Kawaii Planner Scrapbooking Stickers Stationery School Supplies',
+    rating: '4.8',
+    res: 'https://www.aliexpress.com/item/32835020270.html?spm=a2g0s.8937460.0.0.64b62e0e8f737P'    
+  },
+  {
+    image1: 'assets/2.jpg',
+    image2: 'assets/2_2.jpg',
+    image3: 'assets/2_3.jpg',
+    name: 'Beautiful flowers plant washi tape',
+    price: '$2.89',
+    description: '10 Pcs/Box DIY decoration for scrapbooking masking tape adhesive tape',
+    rating: '4.8',
+    res: 'https://www.aliexpress.com/item/32949791910.html?spm=a2g0s.8937460.0.0.64b62e0e8f737P'
+  },
+  {
+    image1: 'assets/3.jpg',
+    image2: 'assets/3_2.jpg',
+    image3: 'assets/3_3.jpg',
+    name: 'Cute Kawaii Fruit Masking Washi Tape',
+    price: '$0.93',
+    description: '1.5cm * 7m DIY Decorative Adhesive Tape For Scrapbooking Decoration',
+    rating: '4.9',
+    res: 'https://www.aliexpress.com/item/32881328749.html?spm=a2g0s.8937460.0.0.64b62e0e8f737P'
+  },
+  {
+    image1: 'assets/4.jpg',
+    image2: 'assets/4_2.jpg',
+    image3: 'assets/4_3.jpg',
+    name: 'Narrow Leaves thin border washi tape',
+    price: '$0.65',
+    description: '0.7cm*10m DIY decoration scrapbooking planner masking tape adhesive tape label sticker',
+    rating: '4.8',
+    res: 'https://www.aliexpress.com/item/32989438774.html?spm=a2g0o.detail.1000060.2.21fb4b5dPDas2R&gps-id=pcDetailBottomMoreThisSeller&scm=1007.14977.148727.0&scm_id=1007.14977.148727.0&scm-url=1007.14977.148727.0&pvid=2464e9d2-16d5-4717-b428-afc1c555f685&_t=gps-id:pcDetailBottomMoreThisSeller,scm-url:1007.14977.148727.0,pvid:2464e9d2-16d5-4717-b428-afc1c555f685,tpp_buckets:668%230%23131923%2318_668%23808%234094%23350_668%23888%233325%236'
+  },
+  {
+    image1: 'assets/5.jpg',
+    image2: 'assets/5_2.jpg',
+    image3: 'assets/5_3.jpg',
+    name: 'Colorful dots',
+    price: '$2.11',
+    description: '336 Pcs/lot Washi Tape Japanese Paper DIY Planner Masking Tape Adhesive Tapes Stickers Decorative Stationery Tapes',
+    rating: '4.1',
+    res: 'https://www.aliexpress.com/item/33002590797.html?spm=a2g0s.8937460.0.0.64b62e0e8f737P'
+  },
+  {
+    image1: 'assets/6.jpg',
+    image2: 'assets/6_2.jpg',
+    image3: 'assets/6_3.jpg',
+    name: 'Animal Flower Paper Masking Tapes',
+    price: '$2.18',
+    description: '3pcs/pack Washi Masking Tape Set Petal Japanese Washi Tape Diy Scrapbooking Sticker, 15mm x 5m',
+    rating: '4.5',
+    res: 'https://www.aliexpress.com/item/32846511721.html?spm=a2g0s.8937460.0.0.64b62e0e8f737P'
+  },
+  {
+    image1: 'assets/7.jpg',
+    image2: 'assets/7_2.jpg',
+    image3: 'assets/7_3.jpg',
+    name: 'Romantic Memories Stickers Set',
+    price: '$1.72',
+    description: '60pcs/pack Decorative Stationery Stickers Scrapbooking DIY Diary Album Stick Label',
+    rating: '4.8',
+    res: 'https://www.aliexpress.com/item/32975421555.html?spm=a2g0s.8937460.0.0.64b62e0e8f737P'
+  },
+  {
+    image1: 'assets/8.jpg',
+    image2: 'assets/8_2.jpg',
+    image3: 'assets/8_3.jpg',
+    name: 'Striped/Grid/Flowers Washi Tape',
+    price: '$2.21',
+    description: '5 pcs/pack Basic Solid Color paper Adhesive Tape DIY Scrapbooking Sticker Label Masking Tape',
+    rating: '4.2',
+    res: 'https://www.aliexpress.com/item/32956429092.html?spm=a2g0s.8937460.0.0.64b62e0e8f737P'
+  },
+  {
+    image1: 'assets/9.jpg',
+    image2: 'assets/9_2.jpg',
+    image3: 'assets/9_3.jpg',
+    name: 'Black Foiled Washi Tape',
+    price: '$1.90',
+    description: '10pcs/set Japanese Paper DIY Planner Masking Tape Adhesive Tapes Stickers Decorative Stationery Tapes',
+    rating: '4.7',
+    res: 'https://www.aliexpress.com/item/32948772110.html?spm=a2g0s.8937460.0.0.64b62e0e8f737P'
+  },
+  { 
+    image1: 'assets/10.jpg',
+    image2: 'assets/10_2.jpg',
+    image3: 'assets/10_3.jpg',
+    name: 'Light And Shadow Diary Stickers',
+    price: '$1.75',
+    description: '40 Pcs/pack For Stationery Scrapbooking Diy Diary Album Bullet Journal Stick Label',
+    rating: '4.4',
+    res: 'https://www.aliexpress.com/item/32948852741.html?spm=a2g0s.8937460.0.0.64b62e0e8f737P'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
